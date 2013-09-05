33476d46be75d5002900d9e085a34ced4b3538cf
noah pryor <noah@noahpryor.com>
2013-09-05 07:34:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WillFilterTest::Application.config.secret_key_base = '0400379bccf999d25e91bb13d29eacd29ca3c05fe1a6eaf4da7c24a6fdaeb2d007bd47641d0e4c142e86ae9f964ba288f89d3ea080f7747ea47b2d387dbf47c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WillFilterTest::Application.config.session_store :cookie_store, key: '_will_filter_test_session'
